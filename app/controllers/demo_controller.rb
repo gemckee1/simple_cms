a433689de2ffbdd9d81c06b1fe1190e621fe952b
Greg McKee <greg.mckee@me.com>
2016-12-04 10:17:53 -0600

    Chapter 3 Study

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,22 @@ class DemoController < ApplicationController
    layout false
 
   def index
+    render('index')
   end
+
+  def Hello
+    @array = [1,2,3,4,5]
+    @id = params['id']
+    @page = params[:page]
+    render('Hello')
+  end
+
+  def other_hello
+    redirect_to(:action => 'Hello')
+  end
+
+    def Lynda
+      redirect_to('http://lynda.com')
+    end
+
 end
